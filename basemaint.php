c84b7a83853c2a74233ff89e67d2760e7b12d14b
ArouG <francois.dantigny@gmail.com>
2013-12-03 10:16:37 +0100

    Update basemaint.php
    après chaque 'scan' on va balayer la table des statistiques dans l'objectif de disposer des minima et maxima nous permettant de dimensionner les graphiques. Ces minima / maxima (nombre de seeders, de leechers et de complétés) seront détaillés par catégorie de torrents et par âges de torrents (les 'vieux', les 'normaux' et les plus 'récents'). Enfin on créera une synthèse regroupant tous les âges

--- basemaint.php
+++ basemaint.php
@@ -97,6 +97,128 @@
     $sth = $db->prepare($sql);
     $sth->execute();
 
+    // sélectionne les catégories par type d'age des torrents
+    $sqn="SELECT B.Uic, A.NbS, A.NbL, A.nbC  FROM scans AS A, base AS B, normals AS C WHERE ((B.Tid=C.Tid) and (C.Tid=A.Tid)) ORDER by B.Uic"; 
+    $sqr="SELECT B.Uic, A.NbS, A.NbL, A.nbC  FROM scans AS A, base AS B, recents AS C WHERE ((B.Tid=C.Tid) and (C.Tid=A.Tid)) ORDER by B.Uic;";   
+    $sqo="SELECT B.Uic, A.NbS, A.NbL, A.nbC  FROM scans AS A, base AS B, oldests AS C WHERE ((B.Tid=C.Tid) and (C.Tid=A.Tid)) ORDER by B.Uic;"; 
+       
+    $sqle="DELETE from synth";    // nettoit tout
+    $sth = $db->prepare($sqle);
+    $sth->execute(); 
+    $sth->CloseCursor();    
+                                                  
+    $sth= $db->prepare($sqn);      // récupère 3 tables en fonctions des 3 populations de torrents
+    $sth->execute();  
+    $Indn = $sth->fetchAll(PDO::FETCH_ASSOC);   
+    $sth= $db->prepare($sqr);
+    $sth->execute();              
+    $Indr = $sth->fetchAll(PDO::FETCH_ASSOC);    
+    $sth= $db->prepare($sqo);
+    $sth->execute(); 
+    $Indo = $sth->fetchAll(PDO::FETCH_ASSOC);     
+    
+    $A=array();   $I=array();    
+    //               Examen première table pour sauvegarder dans synth ce qui se rapporte à la table normals
+    $Sm=0; $SM=0; $Lm=0; $LM=0; $Cm=0; $CM=0; $OldC=0;
+    for ($p=0; $p<count($Indn); $p++){    
+        if ($Indn[$p]['Uic'] != $OldC){          // si on change de catégories
+           if ($p>0){                            // on commence par sauvegarder les valeurs précédentes
+                $sqlp="INSERT INTO synth (Uic, tableau, Smin, Smax, Lmin, Lmax, Cmin, Cmax) VALUES (".$OldC.", 'N',".$Sm.", ".$SM.", ".$Lm.", ".$LM.", ".$Cm.", ".$CM.")";
+                $sth= $db->prepare($sqlp);
+                $sth->execute();  
+                $A[$OldC]=array();
+                $A[$OldC][]=array($Sm,$SM,$Lm,$LM,$Cm,$CM);  
+                $I[]=$OldC; 
+           }                     
+           $OldC=$Indn[$p]['Uic']; $Sm=$Indn[$p]['NbS']; $SM=$Indn[$p]['NbS']; $Lm=$Indn[$p]['NbL']; $LM=$Indn[$p]['NbL']; $Cm=$Indn[$p]['NbC']; $CM=$Indn[$p]['NbC'];
+        } else {                                 // dans la même catégorie, on filtre pour conserver les min et max
+            if ($Indn[$p]['NbS'] < $Sm) $Sm=$Indn[$p]['NbS'];  
+            if ($Indn[$p]['NbS'] > $SM) $SM=$Indn[$p]['NbS'];  
+            if ($Indn[$p]['NbL'] < $Lm) $Lm=$Indn[$p]['NbL'];  
+            if ($Indn[$p]['NbL'] > $LM) $LM=$Indn[$p]['NbL'];  
+            if ($Indn[$p]['NbC'] < $Cm) $Cm=$Indn[$p]['NbC'];  
+            if ($Indn[$p]['NbC'] > $CM) $CM=$Indn[$p]['NbC'];    
+        }
+    }  
+    // sans oublier de sauvegarder la dernière ligne !  
+    $sqlp="INSERT INTO synth (Uic, tableau, Smin, Smax, Lmin, Lmax, Cmin, Cmax) VALUES (".$OldC.", 'N',".$Sm.", ".$SM.", ".$Lm.", ".$LM.", ".$Cm.", ".$CM.")"; 
+    $sth= $db->prepare($sqlp);
+    $sth->execute(); 
+    $A[$OldC][]=array($Sm,$SM,$Lm,$LM,$Cm,$CM); 
+    $I[]=$OldC;           
+    
+    //               Passons à la seconde table : oldests
+    $Sm=0; $SM=0; $Lm=0; $LM=0; $Cm=0; $CM=0; $OldC=0;
+    for ($p=0; $p<count($Indo); $p++){
+        if ($Indo[$p]['Uic'] != $OldC){
+           if ($p>0){
+                $sqlq="INSERT INTO synth (Uic, tableau, Smin, Smax, Lmin, Lmax, Cmin, Cmax) VALUES (".$OldC.", 'O',".$Sm.", ".$SM.", ".$Lm.", ".$LM.", ".$Cm.", ".$CM.")";  
+                $sth= $db->prepare($sqlq);
+                $sth->execute();     
+                if (!in_array($OldC, $I)) $I[]=$OldC;             // $I va lister et indexer toutes les catégories. Ne créer un nouvel enregistrement que si pas encore disponible
+                $A[$OldC][]=array($Sm,$SM,$Lm,$LM,$Cm,$CM); 
+           }                     
+           $OldC=$Indo[$p]['Uic']; $Sm=$Indo[$p]['NbS']; $SM=$Indo[$p]['NbS']; $Lm=$Indo[$p]['NbL']; $LM=$Indo[$p]['NbL']; $Cm=$Indo[$p]['NbC']; $CM=$Indo[$p]['NbC'];
+        } else {
+            if ($Indo[$p]['NbS'] < $Sm) $Sm=$Indo[$p]['NbS'];  
+            if ($Indo[$p]['NbS'] > $SM) $SM=$Indo[$p]['NbS'];  
+            if ($Indo[$p]['NbL'] < $Lm) $Lm=$Indo[$p]['NbL'];  
+            if ($Indo[$p]['NbL'] > $LM) $LM=$Indo[$p]['NbL'];  
+            if ($Indo[$p]['NbC'] < $Cm) $Cm=$Indo[$p]['NbC'];  
+            if ($Indo[$p]['NbC'] > $CM) $CM=$Indo[$p]['NbC'];    
+        }
+    }    
+    $sqlq="INSERT INTO synth (Uic, tableau, Smin, Smax, Lmin, Lmax, Cmin, Cmax) VALUES (".$OldC.", 'O',".$Sm.", ".$SM.", ".$Lm.", ".$LM.", ".$Cm.", ".$CM.")";  
+    $sth= $db->prepare($sqlq);    
+    $sth->execute();    
+    if (!in_array($OldC, $I)) $I[]=$OldC;            
+    $A[$OldC][]=array($Sm,$SM,$Lm,$LM,$Cm,$CM); 
+    
+    //               et enfin la dernière : oldests
+    $Sm=0; $SM=0; $Lm=0; $LM=0; $Cm=0; $CM=0; $OldC=0;
+    for ($p=0; $p<count($Indr); $p++){
+        if ($Indr[$p]['Uic'] != $OldC){
+           if ($p>0){
+                $sqlp="INSERT INTO synth (Uic, tableau, Smin, Smax, Lmin, Lmax, Cmin, Cmax) VALUES (".$OldC.", 'R',".$Sm.", ".$SM.", ".$Lm.", ".$LM.", ".$Cm.", ".$CM.")";  
+                $sth= $db->prepare($sqlp);
+                $sth->execute();    
+                if (!in_array($OldC, $I)) $I[]=$OldC;   
+                $A[$OldC][]=array($Sm,$SM,$Lm,$LM,$Cm,$CM); 
+           }                     
+           $OldC=$Indr[$p]['Uic']; $Sm=$Indr[$p]['NbS']; $SM=$Indr[$p]['NbS']; $Lm=$Indr[$p]['NbL']; $LM=$Indr[$p]['NbL']; $Cm=$Indr[$p]['NbC']; $CM=$Indr[$p]['NbC'];
+        } else {
+            if ($Indr[$p]['NbS'] < $Sm) $Sm=$Indr[$p]['NbS'];  
+            if ($Indr[$p]['NbS'] > $SM) $SM=$Indr[$p]['NbS'];  
+            if ($Indr[$p]['NbL'] < $Lm) $Lm=$Indr[$p]['NbL'];  
+            if ($Indr[$p]['NbL'] > $LM) $LM=$Indr[$p]['NbL'];  
+            if ($Indr[$p]['NbC'] < $Cm) $Cm=$Indr[$p]['NbC'];  
+            if ($Indr[$p]['NbC'] > $CM) $CM=$Indr[$p]['NbC'];    
+        }
+    }         
+    $sqlp="INSERT INTO synth (Uic, tableau, Smin, Smax, Lmin, Lmax, Cmin, Cmax) VALUES (".$OldC.", 'R',".$Sm.", ".$SM.", ".$Lm.", ".$LM.", ".$Cm.", ".$CM.")";  
+    $sth= $db->prepare($sqlp);      
+    $sth->execute();    
+    if (!in_array($OldC, $I)) $I[]=$OldC;  
+    $A[$OldC][]=array($Sm,$SM,$Lm,$LM,$Cm,$CM); 
+        
+    // Dernière boucle pour disposer des mêmes informations mais pour l'ensemble des statistiques : on englobe les trois tables en quelque sorte        
+    for ($p=0; $p<count($I); $p++){            // balayage de chaque catégorie
+              $Sm=$A[$I[$p]][0][0]; $SM=$A[$I[$p]][0][1]; $Lm=$A[$I[$p]][0][2]; $LM=$A[$I[$p]][0][3]; $Cm=$A[$I[$p]][0][4]; $CM=$A[$I[$p]][0][5]; 
+              for ($j=1; $j<count($A[$I[$p]]); $j++){                    // pour chacune on balaye sur l'âge des torrents
+                  if ($A[$I[$p]][$j][0] < $Sm) $Sm=$A[$I[$p]][$j][0]; 
+                  if ($A[$I[$p]][$j][1] > $SM) $SM=$A[$I[$p]][$j][1];  
+                  if ($A[$I[$p]][$j][2] < $Lm) $Lm=$A[$I[$p]][$j][2]; 
+                  if ($A[$I[$p]][$j][3] > $LM) $LM=$A[$I[$p]][$j][3];
+                  if ($A[$I[$p]][$j][4] < $Cm) $Cm=$A[$I[$p]][$j][4]; 
+                  if ($A[$I[$p]][$j][5] > $CM) $CM=$A[$I[$p]][$j][5];
+              }  
+              // et on sauvegarde avec un "tableau" A comme tous (All)
+              $sqlp="INSERT INTO synth (Uic, tableau, Smin, Smax, Lmin, Lmax, Cmin, Cmax) VALUES (".$I[$p].", 'A',".$Sm.", ".$SM.", ".$Lm.", ".$LM.", ".$Cm.", ".$CM.")";  
+              $sth= $db->prepare($sqlp);
+              $sth->execute();               
+    }       
+
+
     // nettoyage final et cloture de la baase !
     $sql="VACUUM";
     $nimp=$db->exec($sql);
